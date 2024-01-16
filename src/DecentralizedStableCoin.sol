a7d0d7a7982bd55d19d571da0478c239e86dbfbf
Spencer Wilfahrt <spencerwilf@yahoo.com>
2024-01-16 14:17:13 -0500

    natspec

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -1,3 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.21;
 
+/**
+ * @title Decentralized Stable Coin
+ * @author Spencer Wilfahrt
+ * Collateral: Exogenous
+ * Minting: Algorithmic
+ * Relative Stability: Pegged to USD
+ * 
+ * @notice This contract is meant to be governed by the DSCEngine. This contract is simply the ERC20 implementation of the stablecoin system.
+ */
+contract DecentralizedStableCoin {
+
+}
