cbfca59838dfa4c57d02cf9a0fe7757579f1bd3c
Spencer Wilfahrt <spencerwilf@yahoo.com>
2024-01-16 14:20:50 -0500

    inheritence

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.21;
 
+import {ERC20, ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
 /**
  * @title Decentralized Stable Coin
  * @author Spencer Wilfahrt
@@ -10,6 +12,7 @@ pragma solidity 0.8.21;
  * 
  * @notice This contract is meant to be governed by the DSCEngine. This contract is simply the ERC20 implementation of the stablecoin system.
  */
-contract DecentralizedStableCoin {
+contract DecentralizedStableCoin is ERC20Burnable {
 
+    
 }
