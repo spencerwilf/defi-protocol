8ba579c76c2a52f8ecb61a4bd7fb2941bd8ea037
Spencer Wilfahrt <spencerwilf@yahoo.com>
2024-01-17 15:19:38 -0500

    helper config

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -4,5 +4,8 @@ pragma solidity 0.8.21;
 import {Script} from "forge-std/Script.sol";
 
 contract HelperConfig is Script {
-    
+    struct NetworkConfig {
+        address wethUsdPriceFeed;
+        address wbtcUsdPriceFeed;
+    }
 }
