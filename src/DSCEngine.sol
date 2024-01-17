451fb1434f58584bedff6b316be1809ed4b20f38
Spencer Wilfahrt <spencerwilf@yahoo.com>
2024-01-17 12:37:20 -0500

    collateral

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -135,6 +135,9 @@ contract DSCEngine is ReentrancyGuard {
     /* Public and External View Functions */
 
     function getAccountCollateralValue(address user) public view returns(uint) {
-
+        for (uint i = 0; i < s_collateralTokens.length; i++) {
+            address token = s_collateralTokens[i];
+            uint amount = s_collateralDeposited[user][token];
+        }
     }
 }
