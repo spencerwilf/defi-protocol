08ac79f198210625a8ec6730992391038442e392
Spencer Wilfahrt <spencerwilf@yahoo.com>
2024-01-17 15:50:44 -0500

    price feed array

--- script/DeployDSC.s.sol
+++ script/DeployDSC.s.sol
@@ -7,10 +7,17 @@ import {DSCEngine} from "../src/DSCEngine.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployDSC is Script {
+
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
+
     function run() external returns (DecentralizedStableCoin, DSCEngine) {
         HelperConfig config = new HelperConfig();
 
         (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc, uint256 deployerKey) = config.activeNetworkConfig();
+
+        tokenAddresses = [weth, wbtc];
+        priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
 
         vm.startBroadcast();
         DecentralizedStableCoin dsc = new DecentralizedStableCoin();
