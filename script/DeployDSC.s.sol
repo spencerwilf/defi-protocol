b78cd44e9f7ebb6b4bc2f062f3171021e7d03570
Spencer Wilfahrt <spencerwilf@yahoo.com>
2024-01-17 14:33:55 -0500

    deploy script

---
+++ script/DeployDSC.s.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.21;
+
+import {Script} from "forge-std/Script.sol";
+
+contract DeployDSC is Script {
+    function run() external returns() {
+        
+    }
+}
